cca6a131780bc5d9beacc152d6ff83d79779cc2e
Beau Elliott <belliott1516@gmail.com>
2022-03-08 12:47:59 -0800

    got doors and buttons connected

--- README.md
+++ README.md
@@ -21,8 +21,22 @@ If you work on more than one feature at a time, you are guaranteed to multiply y
       -section2 to track wins, losses, and total 
       -section3 for any stretch goals that i am able to get to.
 1) **Once we know _why_ we need each element, think about how to implement the "Why" as a "How" (i.e., `resultsEl.textContent = newResults`)**
-  -
+  - 3 images 
+    -why? to show the user the correct door and an emoji to show which one is correct
+    -how? by importing the image and the reseting it everytime it is clicked
+  -3 destination elements 
+    -why? to keep track of wins, losses, and total as they change
+    -how? using the winsEl.textContent = winCount 
+  -3 buttons
+    -why? to allow user to guess.
+    -how? add eventlistener to each.
 1) **Find all the 'events' (user clicks, form submit, on load etc) in your app. Ask one by one, "What happens when" for each of these events. Does any state change?**
+  -user clicks button
+    -increment total increases, and either win or loss increases
+    -the app needs to decide which one is the correct cup
+  -update the DOM
+    -the emoji should show up over the correct door
+    -the wins/losses and total shoudl go up
 1) **Think about how to validate each of your features according to a Definition of Done. (Hint: console.log usually helps here.)**
 1) **Consider what features _depend_ on what other features. Use this dependency logic to figure out what order to complete tasks.**
 
--- app.js
+++ app.js
@@ -1,8 +1,72 @@
 // import functions and grab DOM elements
+const greenDoor = document.getElementById('door1-container');
+const pinkDoor = document.getElementById('door2-container');
+const blueDoor = document.getElementById('door3-container');
+
+const greenButton = document.getElementById('door1');
+const pinkButton = document.getElementById('door2');
+const blueButton = document.getElementById('door3');
+
+const winsEl = document.getElementById('wins');
+const totalEl = document.getElementById('total');
+const lossEl = document.getElementById('total');
 
 // let state
-
+let winCount = 0;
+let totalCount = 0;
 // set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+greenButton.addEventListener('click', () =>{
+//get random door 
+    let correctClick = randomHidingPlace();
+//enter handleGuess function
+    handleGuess('greenDoor', correctClick);
+});
+
+pinkButton.addEventListener('click', () =>{
+  //get random door 
+    let correctClick = randomHidingPlace();
+  //enter handleGuess function
+    handleGuess('pinkDoor', correctClick);
+});
+
+blueButton.addEventListener('click', () =>{
+    //get random door 
+    let correctClick = randomHidingPlace();
+    //enter handleGuess function
+    handleGuess('blueDoor', correctClick);
+});
+
+//create function to reset doors after each click
+function resetStyles() {
+    greenDoor.classList.remove('monster');
+    pinkDoor.classList.remove('monster');
+    blueDoor.classList.remove('monster');
+}
+
+//create array for hiding places
+const hidingPlace = ['door1', 'door2', 'door3'];
+
+function randomHidingPlace() {
+    const random = Math.floor(Math.random() * hidingPlace.length);
+
+    return hidingPlace[random];
+}
+
+function handleGuess(userClick, correctClick) {
+  //reset doors to original image each time
+    resetStyles();
+  //increment total count after every click
+    totalCount++;
+  //increment win count after every correct click
+    if (userClick === correctClick) {
+        winCount++;
+    }
+  // make a correct and incorrect door by randomizing the container
+    const hiddenMonster = document.getElementById(`${correctClick}-container`);
+  // make monster face appear on door
+    hiddenMonster.classList.add('monster');
+  // set your counters to increment accordingly
+    winsEl.textContent = winCount;
+    totalEl.textContent = totalCount;
+    lossEl.textContent = totalCount - winCount;
+}
